63aa238c36e385d5357753fb0ae673dffa7c321a
davidk <kdavid@gmail.com>
2017-05-06 03:47:35 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# tweetdump
+
+### Installation
+
+###### Requires a [golang distribution](https://golang.org/doc/install).
+
+    $ go get github.com/davidk/tweetdump
+
+### Updating
+
+    $ go get -u github.com/davidk/tweetdump
+
+### About
+
+tweetdump unpacks [Anaconda's](https://github.com/ChimeraCoder/anaconda) Tweet structures using the [spew pretty printer](https://github.com/davecgh/go-spew/). Use this tool to debug data from Twitter, or to see all the data that can be had when using Anaconda.
+
+### Usage
+
+    # -- The ConsumerKey and SecretToken options need to be defined
+    # -- in order to talk to the Twitter API
+    # -tweetid is the number at the back of the status/ url
+    # IE: https://twitter.com/jack/status/20 (-tweetid would be 20)
+
+    $ bin/tweetdump -help
+    tweetdump version: 0.01
+      -key string
+        	Twitter API consumer key
+      -secret string
+        	Twitter API consumer secret
+      -tweetid int
+        	Unique ID of a tweet to dump
+      -version
+        	Prints version information
+
+To get a secret token and consumer key, run through the gamut of creating a new
+application [here](https://apps.twitter.com/).
---
+++ tweetdump.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/ChimeraCoder/anaconda"
+	"github.com/davecgh/go-spew/spew"
+	"net/url"
+	"os"
+)
+
+const (
+	VERSION = "tweetdump version: 0.01\n"
+)
+
+var (
+
+	tweetID        int64
+	consumerKey    string
+	consumerSecret string
+	version        bool
+
+)
+
+func init() {
+	flag.Int64Var(&tweetID, "tweetid", 0, "Unique ID of a tweet to dump")
+	flag.StringVar(&consumerKey, "key", "", "Twitter API consumer key")
+	flag.StringVar(&consumerSecret, "secret", "", "Twitter API consumer secret")
+	flag.BoolVar(&version, "version", false, "Prints version information")
+
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, VERSION)
+		flag.PrintDefaults()
+	}
+
+	flag.Parse()
+
+	if version {
+		fmt.Println(VERSION)
+		os.Exit(0)
+	}
+
+	if tweetID == 0 || consumerKey == "" || consumerSecret == "" {
+		fmt.Fprintf(os.Stderr, "Error: tweetID, consumerKey and secretToken need to be defined.\n")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	fmt.Println(VERSION)
+}
+
+func dumpTweet(id int64, api *anaconda.TwitterApi) {
+	v := url.Values{}
+	v.Set("tweet_mode", "extended")
+	fmt.Printf("---- TWEET 🆔: %v ----\n", tweetID)
+	tweet, err := api.GetTweet(id, v)
+
+	if err != nil {
+		fmt.Errorf("Could not get tweet: %v", err)
+	}
+
+	spew.Dump(tweet)
+	fmt.Printf("---- TWEET 🆔: %v ----\n", tweetID)
+}
+
+func main() {
+
+	anaconda.SetConsumerKey(consumerKey)
+	anaconda.SetConsumerSecret(consumerSecret)
+	api := anaconda.NewTwitterApi("", "")
+
+	dumpTweet(tweetID, api)
+
+}
